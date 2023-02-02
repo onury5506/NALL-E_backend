bf0560d385624739cdfd01f5b9d5e4523e854107
onury5506 <onuryildizsai@gmail.com>
2023-02-02 19:45:27 +0300

    feature: nft_page query implemented

--- graphql/resolvers/queries/nft_page.js
+++ graphql/resolvers/queries/nft_page.js
@@ -1,6 +1,17 @@
-export default function nft_page(_, query, { models }) {
+import NFT from "../../../db/models/nft/nft.js"
 
-    //TO-DO
+const pageSize = 10
 
-    return []
+export default async function nft_page(_, query, { models }) {
+
+    const { page } = query
+
+    const count = await NFT.count()
+
+    const start = pageSize * page
+    const end = Math.min(start + pageSize, count)
+
+    console.log({start,end})
+
+    return await NFT.find({ id: { $gte: start, $lt: end } })
 }
