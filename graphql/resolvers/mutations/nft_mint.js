75197f6fcaf7fc927ba339329ce1bd17ac0e6a4f
onury5506 <onuryildizsai@gmail.com>
2023-03-19 17:05:27 +0300

    feature: Moderations model integrated

--- graphql/resolvers/mutations/nft_mint.js
+++ graphql/resolvers/mutations/nft_mint.js
@@ -1,8 +1,11 @@
-import NFT from "../../../db/models/nft/nft.js"
 import web3 from "web3"
+import { GraphQLError } from 'graphql'
+
+import NFT from "../../../db/models/nft/nft.js"
 import { contract, accountAddress, getGasPrice } from "../../../web3/contract.js"
 import { generateAndUpload } from "../../../utils/dalle.js"
 import { log } from "../../../utils/log.js"
+import Moderations from "../../../utils/moderations.js"
 
 export default async function nft_mint(_, query) {
     const {
@@ -11,8 +14,29 @@ export default async function nft_mint(_, query) {
     } = query.input
 
     if (!web3.utils.isAddress(mint_to)) {
-        return -1
+        throw new GraphQLError('Invalid Wallet Address!', {
+            extensions: {
+                code: 'BAD_USER_INPUT',
+            },
+        })
     }
+
+    if (!prompt || prompt.length < 10){
+        throw new GraphQLError('Prompt length must be longer than 10!', {
+            extensions: {
+                code: 'BAD_USER_INPUT',
+            },
+        })
+    }
+
+    if (await Moderations(prompt)){
+        throw new GraphQLError('Harmful Content!', {
+            extensions: {
+                code: 'BAD_USER_INPUT',
+            },
+        })
+    }
+
     const gas = await contract.methods.mint(mint_to).estimateGas({ from: accountAddress })
     const gasPrice = Math.floor(parseInt(await getGasPrice())*1.1)
 
@@ -39,7 +63,6 @@ export default async function nft_mint(_, query) {
         newNft.save()
 
         log("NFT Minted",{id,mint_to,image_url:res.Location})
-
     })
 
     return newNft.id
